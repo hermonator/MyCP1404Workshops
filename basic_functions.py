13a79464cb1f7591a654528e3ba846029b73e1ed
Jesse Hermon <jesse.hermon@my.jcu.edu.au>
2016-03-22 10:29:51 +1000

    Updated get_limits to include error checking

--- basic_functions.py
+++ basic_functions.py
@@ -13,12 +13,15 @@ __author__ = 'Lindsay Ward'
 def main():
     lowest, highest = get_limits()
     print("lowest =", lowest, "highest =", highest)
-    print_between(lowest, highest)
+    #print_between(lowest, highest)
 
 
 def get_limits():
     minimum = int(input("Enter the minimum: "))
-    maximum = int(input("Enter the maximum: "))
+    maximum = int(input("Enter the maximum ({} or above): ".format(minimum)))
+    while maximum < minimum:
+        print('Maximum too low!')
+        maximum = int(input("Enter the maximum ({} or above): ".format(minimum)))
     return minimum, maximum
 
 
